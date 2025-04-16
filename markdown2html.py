827bf34dbd8a6c10433dd43661957b78049a544e
AxelNAY <axelnay@sfr.fr>
2025-04-16 09:18:29 +0200

    change the second if in the main

--- markdown2html.py
+++ markdown2html.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if len(sys.argv) < 3:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         exit(1)
-    if os.path.exists(sys.argv[1]) is False:
+    if not os.path.exists(sys.argv[1]):
         sys.stderr.write("Missing " + sys.argv[1] + "\n")
         exit(1)
     exit(0)
