724f44b0b06b5eb047b5aae26df2597a3da2ea29
AxelNAY <axelnay@sfr.fr>
2025-04-16 11:07:05 +0200

    add an heading markdown

--- markdown2html.py
+++ markdown2html.py
@@ -5,6 +5,7 @@ First argument is the name of the Markdown file
 Second argument is the output file name"""
 
 import os
+import re
 import sys
 
 if __name__ == "__main__":
@@ -14,4 +15,19 @@ if __name__ == "__main__":
     if not os.path.exists(sys.argv[1]):
         sys.stderr.write("Missing " + sys.argv[1] + "\n")
         exit(1)
+
+    with open('README.md', 'r') as file:
+        lines = file.readlines()
+
+    with open('README.md', 'w') as file:
+        for line in lines:
+            length = len(line)
+            headings = line.lstrip('#')
+            heading_count = length - len(headings)
+
+            if 1 <= heading_count <= 6:
+                line = '<h{}>'.format(heading_count) + headings.strip() + '</h{}>\n'.format(heading_count)
+
+            file.write(line)
+
     exit(0)
